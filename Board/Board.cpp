19f5f387abd9fc7bdff2de43a44a7f0e6b0592df
Piijas <francois.denquin@gmail.com>
2014-11-12 13:54:34 +0100

    Update Board.cpp

--- Board/Board.cpp
+++ Board/Board.cpp
@@ -1,28 +1,48 @@
 #include "Board.h"
 
+
+/** 
+* Constructor which puts listCard to NULL. 
+*/
 Board::Board(){
   
   listCard = NULL;
 
 }
 
+
+/** 
+* Create a board and initialize it with the pointer of listCard.  
+*/
 Board::Board(std::list<Card> *listCard){
   
   this.listCard = listCard;
 
 }
 
-Card Board::getCardX(unsigned int x){
+
+/** 
+* Get the card which is at the Xth position.
+* @param unsigned int x position of the wanted card.
+* @return Card the pointer of the card.
+*/
+Card * Board::getCardX(unsigned int x){
   
   std::list<Card>::iterator it;
   
   it = listCard.begin();
   it += x;
   
-  return it;  
+  return *it;  
 
 }
 
+
+/** 
+* Take the card which is at the Xth position and erase it of the deck.
+* @param unsigned int x position of the wanted card.
+* @return Card the pointer of the card.
+*/
 Card * takeCardX(unsigned int x){
   
   std::list<Card>::iterator it;
@@ -38,18 +58,31 @@ Card * takeCardX(unsigned int x){
   
 }
 
+
+/** 
+* Return the size of the list.
+*/
 unsigned int getSize(){
   
   return listCard.size;
   
 }
 
+
+/** 
+* Return the maximum size of the list.  
+*/
 unsigned int getMaxSize(){
   
   return listCard.max_size;
   
 }
 
+
+/** 
+* Set the maximum size to the value size.  
+* @param unsigned int size
+*/
 void setMaxSize(unsigned int size){
   
   listCard.max_size = size;
