b09839a92fe1f78e184eda71c5b91c95f6007fd4
Piijas <francois.denquin@gmail.com>
2014-11-16 18:13:35 +0100

    Update Board.h

--- Board/Board.h
+++ Board/Board.h
@@ -13,17 +13,68 @@ class Board
 {
 public:
 	
+	/** 
+	* Constructor which puts listCard to NULL. 
+	*/
 	Board();
+	
+	/** 
+	* Create a board and initialize it with the pointer of listCard.  
+	* @param std::list<Card> *listCard pointer of a list of card.
+	*/
 	Board(std::list<Card> *listCard);
 	
+	/** 
+	* Get the card which is at the Xth position.
+	* @param unsigned int x position of the wanted card.
+	* @return Card the pointer of the card.
+	*/
 	Card * getCardX(unsigned int x);
+	
+	/** 
+	* Take the card which is at the Xth position and erase it of the deck.
+	* @param unsigned int x position of the wanted card.
+	* @return Card the pointer of the card.
+	*/
 	Card * takeCardX(unsigned int x);
+	
+	/**
+	 * Delete the card which is at the Xth position.
+	 * @param unsigned int x position of the card to be deleted.
+	 */
 	void deletCardX(unsigned int x);
+	
+	/** 
+	* Return the size of the list.
+	*/
 	unsigned int getSize();
+	
+	/** 
+	* Return the maximum size of the list.  
+	*/
 	unsigned int getMaxSize();
+	
+	/** 
+	* Set the maximum size of the list to the value size.  
+	* @param unsigned int size.
+	*/
 	void setMaxSize(unsigned int size);
+	
+	/**
+ 	* Add a card at the Xth position of a board.
+ 	* @param Card *card pointer of the target card, unsigned int x wanted position.
+ 	*/
 	void addCardX(Card *card,unsigned int x);
+	
+	/**
+ 	* Add a list of cards at the Xth position of a board.
+ 	* @param std::list<Card> *listCard pointer of the list of cards, unsigned int x wanted position. 
+	 */
 	void addListCardX(std::list<Card> *listCard,unsigned int x);
+	
+	/**
+ 	* Destructor.
+ 	*/
 	~Board();
 
 private:
