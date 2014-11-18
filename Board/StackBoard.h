c6f8532f4fad338d9c82d9d789beeba871b28af9
Piijas <francois.denquin@gmail.com>
2014-11-18 18:49:50 +0100

    Update StackBoard.h

--- Board/StackBoard.h
+++ Board/StackBoard.h
@@ -3,9 +3,21 @@
 
 #include "Board.h"
 
+
+/**
+ * 
+ * Write the functions in StackBoard.cpp, then write here the commentaries
+ * 
+ */
 Class StackBoard : public Board
 {
 public:
+
+  Card * takeCardX(unsigned int x);
+  void deletCardX(unsigned int x);
+  void addCardX(Card *card,unsigned int x);
+  void addListCardX(std::list<Card> *listCard,unsigned int x);
+  ~StackBoard();
 
 private:
 
