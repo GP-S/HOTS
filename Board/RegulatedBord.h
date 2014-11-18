819cd7f71e59acceffaddacf9091b5448d6c7ba6
Piijas <francois.denquin@gmail.com>
2014-11-18 18:51:42 +0100

    Update RegulatedBord.h

--- Board/RegulatedBord.h
+++ Board/RegulatedBord.h
@@ -3,9 +3,23 @@
 
 #include "Board.h"
 
+/**
+ * 
+ * Write the functions in StackBoard.cpp, then write here the commentaries
+ * 
+ */
+
+
 Class RegulatedBoard : public Board
 {
 public:
+  
+  Card * takeCardX(unsigned int x);
+  void deletCardX(unsigned int x);
+  void addCardX(Card *card,unsigned int x);
+  void addListCardX(std::list<Card> *listCard,unsigned int x);
+  ~StackBoard();
+  
   
 private: 
 
