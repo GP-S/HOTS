07baebe15af3c289eebd8419fdd81e57ebf1df07
diconico07 <nicolas.belouin@heptaoctet.net>
2014-11-19 18:35:17 +0100

    Cleaning the old POC files

--- HOTS4thewin/mecaJeu.h
+++
@@ -1,4 +0,0 @@
-void play(Personnage * J1, Personnage * J2);
-void useCard(Personnage * J1, int choix, Personnage * J2);
-bool attaque(Personnage * J1, int choix, Personnage * J2);
-void grabList(Personnage * J1, Personnage * J2, float tab[8]);
--- HOTS4thewin/reseau.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class reseau
-{
-public:
-	reseau();
-	~reseau();
-};
-
