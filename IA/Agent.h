eb8fa987e8d0b72b761c222ab33660d7d7d1a55d
GP-S <deep_blue_@hotmail.fr>
2014-12-11 16:21:53 +0100

    utils : add time and place mask for agen/effect/GE ia : add the header of agent

---
+++ IA/Agent.h
@@ -0,0 +1,46 @@
+#ifndef AGENT_H
+#define AGENT_H
+
+#include <string>
+#include "usefulStruc.inc"
+
+
+/**
+* Class Agent .
+* @author Thibault
+* @resume PickUp the better list of game's instruction
+* in order to satisfy his own mission and rate its action
+**/
+
+class Agent
+{
+public:
+	/**
+	* Default constructor
+	*
+	**/
+	Agent();
+	/**
+	* Default destructor
+	*
+	**/
+	virtual ~Agent();
+	/*
+	* Search the best combination of action in a list of state
+	* resulting for different actions
+	**/
+	void search(/*some list of state*/);
+
+protected:
+	/**
+	* Auto evaluation of th agent
+	* @return his mark(which can be negative)
+	**/
+	int autoEval();
+
+	int satisfaction;
+	const std::string order; //destroy, save, protect, powerup...
+	const placeMask targetAvailable;
+};
+
+#endif //AGENT_H
