26c29cd564622e9ac57d93df8d2eb097a1f42e5f
moonrim <nicolas.leger62@gmail.com>
2014-12-29 19:37:33 +0100

    WIP CreateDeck_Test

--- Test/CreateDeck/CreateDeck_Test.cpp
+++ Test/CreateDeck/CreateDeck_Test.cpp
@@ -53,8 +53,8 @@ int main(int argc, char **argv){
 
 //	foreach(std::list<Card*>, deck, it){
 	for(std::list<Card*>::iterator it = deck->begin(); it!=deck->end(); ++it){
-		std::cout << "*it: " << *it << std::endl;
-		std::cout << " it: " <<  it << std::endl;
+		std::cout << "*it: " << (*it) << std::endl;
+		std::cout << " it: " << ( it) << std::endl;
 
 		printCard(it);
 	}
